09bcc92daf379f341825da91f617cf4ddcca35f7
Dennis Bracklow <DBR@ivu.de>
2019-12-02 15:34:32 +0100

    add constructor(Date, Date, String)

--- AnnisDenka_Management/MailReceiver/src/main/java/Expanse.java
+++ AnnisDenka_Management/MailReceiver/src/main/java/Expanse.java
@@ -1,16 +1,31 @@
+import java.util.Date;
 
 public class Expanse {
+	
+	//testing
+	private Date date_driven_t;
+	private Date date_payment_t;
 	
 	private String date_driven;
 	private String date_payment;
 	private String amountToPay;
 	
+	
+	// uses Date, Date, String
+	public Expanse(Date date_driven_t, Date date_payment_t, String amountToPay) {
+		this.date_driven_t = date_driven_t;
+		this.date_payment_t = date_payment_t;
+		this.amountToPay = amountToPay;
+	}
+	
+	// uses String, String, String
 	public Expanse(String date_driven, String date_payment, String amountToPay) {
 		this.date_driven = date_driven;
 		this.date_payment = date_payment;
 		this.amountToPay = amountToPay;
 	}
 
+	
 	public String getDate_driven() {
 		return date_driven;
 	}
