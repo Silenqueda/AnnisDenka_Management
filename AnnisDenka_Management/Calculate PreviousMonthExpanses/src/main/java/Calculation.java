87c6dc3ba17c45acc4c9e2d1aaf4d3d0ef9901a5
Dennis Bracklow <DBR@ivu.de>
2019-12-02 09:17:23 +0100

    new Project -> CalculatePreviousMonthExpanses

---
+++ AnnisDenka_Management/Calculate PreviousMonthExpanses/src/main/java/Calculation.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * Use of this class is to calculate the previous month
+ * @author DBR *
+ */
+
+public class Calculation {
+	
+	
+	
+	
+
+	public Calculation() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
