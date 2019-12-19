b9884da53ea1b97f0595ae112089549895eee675
Dennis Bracklow <DBR@ivu.de>
2019-12-19 11:29:19 +0100

    add functin to get rid of blank lines in file to provide clean data sets

--- AnnisDenka_Management/MailReceiver/src/main/java/Calculation/DataGatherer.java
+++ AnnisDenka_Management/MailReceiver/src/main/java/Calculation/DataGatherer.java
@@ -36,12 +36,12 @@ public class DataGatherer {
 			readFile = new BufferedReader(new FileReader(home + mailDataPath));
 			String line = readFile.readLine();
 			while(line != null) {
-				wholeDownloadedDataSet.add(buildExpanseFromFile(line));
+				if (line.length()>0) { // this gets rid of blank lines in file
+					wholeDownloadedDataSet.add(buildExpanseFromFile(line));
+				}
 				line = readFile.readLine();
 			}
 			readFile.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
@@ -50,13 +50,30 @@ public class DataGatherer {
 	//separates Line from file into expanse variables
 	private Expanse buildExpanseFromFile(String line) {
 		String date_driven_t = "", date_payment_t = "", amountToPay = "";		
-		String[] separatedData = line.split("\t");
+		String[] separatedData = splitData(line);
 		
 		date_driven_t  = separatedData[0];
 		date_payment_t = separatedData[1];
 		amountToPay    = separatedData[2];
 		
 		return new Expanse(date_driven_t, date_payment_t, amountToPay);
+	}
+	
+	private String[] splitData(String line) {
+		String[] splittedString = null;
+		
+		String deleteSpacesFromLine = line.trim();
+		
+		if(line.contains("\t")) {
+			splittedString = deleteSpacesFromLine.split("\t");
+		} else if(line.contains(";")) {
+			splittedString = deleteSpacesFromLine.split(";");
+		} else {
+			System.out.println("Wrong value separator. '\t' or ';' are allowed.");
+			System.exit(0);
+		}
+		
+		return splittedString;
 	}
 	
 	//TODO: separate content of wholeDownloadedDataSet by Months
